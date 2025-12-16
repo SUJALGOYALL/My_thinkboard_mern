8bb4564eda35e5d660149eca8aaf2dff7a001eb9
SUJALGOYALL <sujal.230104020@iiitbh.ac.in>
2025-12-16 05:58:15 +0530

    Fix case file import

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,55 +9,52 @@ import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
-  const [notes , setNotes] = useState([]);
-  const [Loading , setLoading] = useState(false);
-  
-  useEffect(()=>{
-    const fetchNotes = async()=> {
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
       try {
         const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
       } catch (error) {
-        console.log("error fetching notes");
-        if(error.response.status === 429) {
+        console.log("Error fetching notes");
+        console.log(error.response);
+        if (error.response?.status === 429) {
           setIsRateLimited(true);
-      }else{
-        toast.error("Error fetching notes");
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
       }
-    } finally{
-      setLoading(false);
-    }
     };
+
     fetchNotes();
   }, []);
 
-
-
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <Navbar />
 
-      {isRateLimited && <RateLimitedUi />}
+      {isRateLimited && <RateLimitedUI />}
 
-      <div className = "max-w-7xl mx-auto p-4 mt-6">
-        {Loading && <div className = "text-center text-primary py-10">Loading notes...</div>}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
         {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
-        {notes.length>0 && !isRateLimited &&(
-          <div className = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {notes.map(note =>(
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
               <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
-            </div>
+          </div>
         )}
       </div>
-
-
     </div>
-  )
-}
-
-export default HomePage
+  );
+};
+export default HomePage;
